38bd18c798f56b90bd3cd2c3c2514866e0ec6b73
meldy2503 <emelder.charles25@gmail.com>
2022-06-12 09:08:25 +0100

    style: set global style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import { Fragment } from "react";
 import Layout from "./components/Layout";
+import { GlobalStyle } from "./GlobalStyle";
+import { ThemeProvider } from "styled-components";
 
 function App() {
+  const theme = {
+    media: {
+      smobile: "420px",
+      mobile: "768px",
+      tablet: "960px",
+      desktop: "1440px",
+    },
+  };
+
   return (
-    <Fragment>
-      <Layout />
-    </Fragment>
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <GlobalStyle />
+        <Layout />
+      </Fragment>
+    </ThemeProvider>
   );
 }
 
--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,5 +1,46 @@
-// body {
-//     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-//       "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-//       sans-serif;
-//   }
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*,
+*::before,
+*::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-size: 1.6rem;
+    color: #00102e;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen","Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif; 
+    }
+    
+    
+ html {
+   font-size: 62.5%;
+   scroll-behavior: smooth;
+   
+  }
+
+@media only screen and (max-width: 96em) {
+  html {
+    font-size: 60%;
+  }
+}
+@media only screen and (max-width: 75em) {
+  html {
+    font-size: 58%;
+  }
+}
+
+a {
+  text-decoration: none;
+}
+
+ul {
+  list-style-type: none;
+}
+
+
+
+`;
