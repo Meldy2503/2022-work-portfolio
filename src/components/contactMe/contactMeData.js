9f73a1f29609f47a64436473c8c33242fe537390
meldy2503 <emelder.charles25@gmail.com>
2022-06-13 07:47:06 +0100

    feat: add data file to contact section

---
+++ src/components/contactMe/contactMeData.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { AiOutlineMail, AiOutlineGithub, AiFillLinkedin } from "react-icons/ai";
+
+export const FormInputs = [
+  {
+    id: 1,
+    type: "text",
+    name: "name",
+    idty: "name",
+    placeholder: "Full name",
+    value: "name",
+  },
+  {
+    id: 2,
+    type: "email",
+    name: "email",
+    idty: "email",
+    placeholder: "Email",
+    value: "email",
+  },
+  {
+    id: 3,
+    type: "tel",
+    name: "phone",
+    idty: "phone",
+    placeholder: "Phone number",
+    value: "phone",
+  },
+  {
+    id: 4,
+    type: "text",
+    name: "subject",
+    idty: "subject",
+    placeholder: "Subject",
+    value: "subject",
+  },
+];
+
+// export const MediaData = [
+//   {
+//     id: 1,
+//     icon: <AiOutlineMail />,
+//     ref: "mailto:emelder.charles25@gmail.com",
+//     connect: "emelder.charles25@gmail.com",
+//   },
+//   {
+//     id: 2,
+//     ref: "https://github.com/meldy2503/",
+//     icon: <AiOutlineGithub />,
+//     connect: "meldy2503",
+//   },
+//   {
+//     id: 3,
+//     ref: "https://www.linkedin.com/in/emelder-okafor-67b22122b",
+//     icon: <AiFillLinkedin />,
+//     connect: "emelder-okafor",
+//   },
+// ];
