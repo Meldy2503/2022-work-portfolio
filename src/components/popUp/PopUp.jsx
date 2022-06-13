8b4271445db5730c5f74d02fce55d4b8d34788e6
meldy2503 <emelder.charles25@gmail.com>
2022-06-14 00:28:32 +0100

    feat: add popup page

--- src/components/popUp/PopUp.jsx
+++ src/components/popUp/PopUp.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Button } from "../reuseableComponents/buttonStyle";
+import { PopMessage, Container } from "./popUpStyle";
+import thumbsUp from "../../img/thumbs-up.png";
 
 const PopUp = () => {
-  return <div>PopUp</div>;
+  return (
+    <Container>
+      <PopMessage>
+        <div>
+          <img src={thumbsUp} alt="emoji" />
+          <p>
+            Thanks for reaching out ! <br /> I will get back to you as soon as
+            possible
+          </p>
+          <Button href="/">OK</Button>
+        </div>
+      </PopMessage>
+    </Container>
+  );
 };
 
 export default PopUp;
